d713417017ea9827c1678747fcdb51e6a187cd90
Saif Ali <saifali87211@gmail.com>
2024-05-27 22:35:21 +0530

    setup util code

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ import connectDb from "./db/db.js";
 
 dotenv.config({ path: "./env" });
 
-connectDb();
+connectDb()
+  .then(() => {
+
+    app.on("error",(error)=>{
+        console.log("Error on App: ",error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`server is running at port ${process.env.PORT}`)
+    );
+  })
+  .catch((error) => console.log("DB Connection Error!!! ", error));
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asynHandler = (reqestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(reqestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asynHandler };
