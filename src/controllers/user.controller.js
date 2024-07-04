95f1771b8700cb510fa4011b4efe32fbbab09059
Saif Ali <saifali87211@gmail.com>
2024-07-04 23:16:58 +0530

    add getWatchHistory end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -366,6 +366,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getUserWathcHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+      $lookup: {
+        from: "videos",
+        localField: "watchHistroy",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched succesfully."
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -377,4 +429,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getUserWathcHistory,
 };
